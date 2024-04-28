9ebbde0ed3f0c5c1fe41a10c1d18928753525aaa
saikrishna-682 <ss682@njit.edu>
2024-04-28 14:52:09 -0400

    slight changes near the azure storage folder path

--- DataToAzureStorageContainer/pushToAzureStorage.py
+++ DataToAzureStorageContainer/pushToAzureStorage.py
@@ -6,8 +6,11 @@ from config import ACCOUNT_NAME, ACCOUNT_KEY, CONTAINER_NAME, DESTINATION_FOLDER
 
 # Connect to Azure Storage
 blob_service_client = BlobServiceClient.from_connection_string(f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net")
+# print(blob_service_client)
+# container_list = blob_service_client.list_containers()
+# for container in container_list:
+#     print(container.name)
 container_client = blob_service_client.get_container_client(CONTAINER_NAME)
-
 folder_path = "/Users/saikrishnasomavarapu/Documents/pycharm-projects/Covid-19/csv_data/"
 
 folder_files = os.listdir(folder_path)
@@ -17,13 +20,18 @@ pd.set_option('display.expand_frame_repr', False)
 
 for file_names in folder_files:
     file_name = os.path.join(folder_path, file_names)
-    file_path_name = file_name.split('.')[0]
+    # print("=========== file_names",file_names)
+    file_path_name = file_names.split('.')[0]
+    print(file_path_name)
     data = pd.read_csv(file_name)
     # Drop rows with any missing values
     data = data.dropna(how='any')
     print(data.to_string(index=False))
     # Upload the file to Azure Storage
-    blob_client = container_client.get_blob_client(f"{DESTINATION_FOLDER}/{file_names}")
+    blob_client = container_client.get_blob_client(f"{DESTINATION_FOLDER}/{file_path_name}/{file_names}")
     with open(file_name, "rb") as data:
-        blob_client.upload_blob(data)
-    break
+        # print(data)
+        # blob_client.upload_blob(data)
+        break
+
+
